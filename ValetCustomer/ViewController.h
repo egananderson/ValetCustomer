d5b8036fb4da8c9486b3aa0c90fadbf103893cef
Egan Anderson <p.egan.anderson@gmail.com>
2015-05-27 16:23:41 -0600

    Initial Commit

---
+++ ValetCustomer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ValetCustomer
+//
+//  Created by Egan Anderson on 5/27/15.
+//  Copyright (c) 2015 Egan Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ValetCustomer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ValetCustomer
+//
+//  Created by Egan Anderson on 5/27/15.
+//  Copyright (c) 2015 Egan Anderson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
